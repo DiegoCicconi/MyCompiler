2e307029d8bdc19d6ec88fb3a29c798ac0d9b5d8
Diego Cicconi <diego.cicconi@gmail.com>
2016-11-07 17:33:37 -0300

    grammar compilation script deleted

--- src/Gramatica/CompilarMyG.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Hello, compiling now..."
-./yacc.exe -Jnorun -Jnodebug -Jthrows=IOException -Jsemantic=SymbolItem MyGrammar.y
-echo "done"
-read -s -n 1 -p "Press any key to continue . . ."
